140729ca613844223ab47da23daa9807c351c9c7
Pradeva <90541409+Pradeva@users.noreply.github.com>
2025-03-18 10:10:45 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:itdp/requestcreditpage.dart';
 import 'splash_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: const RequestCreditPage(),
+    home: SplashScreen(),
   ));
 }
